b557cc682688240025f2a8b7efb147d6131b580c
HariPanicker <harikrishnapanicker@gmail.com>
2022-01-26 08:24:11 +0530

    Puzzle

---
+++ MVVM/MVVM/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by HariPanicker on 25/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func smtng() {
+        let string = "213343313"
+        let ww = ["1","3","5","7","9"]
+        var m = 
+    }
+}
+
